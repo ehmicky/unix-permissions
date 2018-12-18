77c5aa8c4ba6225d00cccaa50f73df2d2fb47cea
ehmicky <ehmicky@gmail.com>
2018-12-18 17:00:00 +0200

    Add isValid()

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 'use strict'
 
-const { getType } = require('./parse')
+const assert = require('assert')
+
+const TYPES = require('./types')
+
+const getType = function(perm) {
+  return TYPES.find(({ test }) => test(perm))
+}
+
+const isValid = function(perm) {
+  return getType(perm) !== 'invalid'
+}
+
+const parse = function(perm) {
+  const type = getType(perm)
+
+  assert(type.name !== 'invalid', `Invalid permissions: ${perm}`)
+
+  const tokens = type.parse(perm)
+  return tokens
+}
 
 module.exports = {
   getType,
+  isValid,
+  parse,
 }
--- test/main.js
+++ test/main.js
@@ -2,8 +2,9 @@
 
 const test = require('ava')
 
-const { getType } = require('../localpack')
+const { getType, isValid } = require('../localpack')
 
 test('Dummy test', t => {
   t.is(typeof getType, 'function')
+  t.is(typeof isValid, 'function')
 })
