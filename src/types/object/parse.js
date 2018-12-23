48e433b42e622a056ce3dec41470cd5637146953
ehmicky <ehmicky@gmail.com>
2018-12-23 17:00:00 +0200

    Fix import

--- src/types/object/parse.js
+++ src/types/object/parse.js
@@ -1,12 +1,9 @@
 'use strict'
 
 const { isPlainObject } = require('../../utils')
+const { CATEGORY_PERMISSIONS } = require('../../constants')
 
-const {
-  SHORT_CATEGORIES,
-  SHORT_PERMISSIONS,
-  CATEGORY_PERMISSIONS,
-} = require('./constants')
+const { SHORT_CATEGORIES, SHORT_PERMISSIONS } = require('./constants')
 
 const name = 'object'
 
