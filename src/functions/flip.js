7d1b5752be59549c88ff22ad9879d94dce47cfd7
ehmicky <ehmicky@gmail.com>
2018-12-26 17:00:00 +0200

    Change invert() and flip()

--- src/functions/flip.js
+++ src/functions/flip.js
@@ -1,12 +1,20 @@
 'use strict'
 
 const { unaryMap } = require('../helpers')
-
-const { fullMap } = require('./full')
-const { notMap } = require('./not')
+const { NODES_MAP } = require('../nodes')
+const { omitBy, mapValues } = require('../utils')
 
 const flipMap = function(nodesMap) {
-  return notMap(fullMap(nodesMap))
+  const flippedMap = omitBy(
+    NODES_MAP,
+    (node, nodeKey) => nodesMap[nodeKey] !== undefined,
+  )
+  const flippedMapA = mapValues(flippedMap, addAdd)
+  return flippedMapA
+}
+
+const addAdd = function(node) {
+  return { ...node, add: true }
 }
 
 const flip = unaryMap.bind(null, flipMap)
--- src/functions/invert.js
+++ src/functions/invert.js
@@ -1,23 +1,19 @@
 'use strict'
 
 const { unaryMap } = require('../helpers')
-const { mapValues } = require('../utils')
+const { omitBy } = require('../utils')
 const { SPECIAL_PERMISSIONS } = require('../constants')
 
 const { flipMap } = require('./flip')
 
 const invertMap = function(nodesMap) {
   const nodesMapA = flipMap(nodesMap)
-  const nodesMapB = mapValues(nodesMapA, removeSpecial)
+  const nodesMapB = omitBy(nodesMapA, isSpecial)
   return nodesMapB
 }
 
-const removeSpecial = function(node) {
-  if (!SPECIAL_PERMISSIONS.includes(node.permission)) {
-    return node
-  }
-
-  return { ...node, add: false }
+const isSpecial = function({ permission }) {
+  return SPECIAL_PERMISSIONS.includes(permission)
 }
 
 const invert = unaryMap.bind(null, invertMap)
