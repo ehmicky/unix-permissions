3281c8f992065b130b9a4b190c399d890bc0bf75
ehmicky <ehmicky@gmail.com>
2018-12-28 17:00:00 +0200

    Change `type()` invalid constant

--- src/type.js
+++ src/type.js
@@ -6,10 +6,10 @@ const type = function(perm) {
   const typeA = Object.entries(TYPES_MAP).find(
     ([, { parse }]) => parse(perm) !== undefined,
   )
-  return typeA === undefined ? UNKNOWN_TYPE : typeA[0]
+  return typeA === undefined ? INVALID_TYPE : typeA[0]
 }
 
-const UNKNOWN_TYPE = 'unknown'
+const INVALID_TYPE = 'invalid'
 
 module.exports = {
   type,
