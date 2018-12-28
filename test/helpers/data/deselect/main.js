8670025868fded2fd6afe4fcaf8542c13b39acc5
ehmicky <ehmicky@gmail.com>
2018-12-28 17:00:00 +0200

    Fix code duplication

--- test/helpers/data/deselect/main.js
+++ test/helpers/data/deselect/main.js
@@ -5,8 +5,8 @@ const { forEachDataType, forEachCategory } = require('../repeat')
 const octal = require('./octal')
 const number = require('./number')
 const stat = require('./stat')
-const symbolic = require('./symbolic')
 const object = require('./object')
+const symbolic = require('./symbolic')
 
 const DESELECT_DATA = forEachCategory(
   forEachDataType({ number, octal, stat, symbolic, object }),
