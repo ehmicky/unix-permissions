bf9adbadcf9c3e975e150a255818310e46085ebd
ehmicky <ehmicky@gmail.com>
2018-12-28 17:00:00 +0200

    Add comments

--- src/functions/min_max.js
+++ src/functions/min_max.js
@@ -3,15 +3,24 @@
 const { variableMap } = require('../helpers')
 const { mapValues, omitBy } = require('../utils')
 
+// Returns the minimum|maximum permissions among two permissions.
+// This is done permission bit by permission bit.
 const minMaxMap = function(values, nodesMap, nodesMapA) {
   const mergedNodes = { ...nodesMap, ...nodesMapA }
   const nodesMapB = mapValues(mergedNodes, (node, nodeKey) =>
     findNode({ values, nodesMap, nodesMapA, nodeKey }),
   )
+  // `undefined` nodes might be present if they got picked because of being
+  // higher|lower than `+` or `-`
   const nodesMapC = omitBy(nodesMapB, node => node === undefined)
   return nodesMapC
 }
 
+// Omitted permissions depend on the permission they will be applied to, i.e.
+// can be either `+` or `-`. In that case, they stand between `+` and `-`
+// in comparison order.
+// We iterate over both nodes and the possible values from highest to lowest
+// until finding the right nore.
 const findNode = function({ values, nodesMap, nodesMapA, nodeKey }) {
   const [nodeB] = values
     .flatMap(value => [[nodesMap[nodeKey], value], [nodesMapA[nodeKey], value]])
