bb478854f9f16eab6e6e245d07fc4c533625aadc
ehmicky <ehmicky@gmail.com>
2018-12-28 17:00:00 +0200

    Add comments

--- src/types/symbolic/join.js
+++ src/types/symbolic/join.js
@@ -2,19 +2,24 @@
 
 const { CATEGORIES } = require('../../constants')
 
+// Join categories, e.g. `u+x,g+x` -> `ug+x`
 const joinCategories = function(node, index, nodes) {
   const sameNodes = nodes.filter(nodeA => canJoinNodes(node, nodeA))
 
+  // Nothing to join
   if (sameNodes.length === 1) {
     return node
   }
 
   const categories = sameNodes.map(nodeA => nodeA.category)
 
+  // This function iterates over all nodes. We only do the join for the first
+  // node, not the following nodes, since they would already be joined.
   if (categories[0] !== node.category) {
     return []
   }
 
+  // `u+x,g+x,o+x` -> `a+x`
   if (categories.length === CATEGORIES.length) {
     return { ...node, category: 'a' }
   }
