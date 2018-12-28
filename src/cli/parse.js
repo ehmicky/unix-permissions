5d38831d8806f82e34e21c47bd9fc2932ed470d8
ehmicky <ehmicky@gmail.com>
2018-12-28 17:00:00 +0200

    Improve CLI

--- src/cli/parse.js
+++ src/cli/parse.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const parseConfig = function({ yargs }) {
-  // eslint-disable-next-line id-length
-  const { _: command, permission, permissions = [] } = yargs.parse()
+  const {
+    // eslint-disable-next-line id-length
+    _: [command],
+    permission,
+    permissions = [],
+  } = yargs.parse()
 
   const args = [permission, ...permissions].filter(Boolean).map(String)
   return { command, args }
--- src/cli/top.js
+++ src/cli/top.js
@@ -5,14 +5,26 @@ const yargs = require('yargs')
 const COMMANDS = require('./commands')
 
 const defineCli = function() {
-  const yargsA = yargs.usage(USAGE).strict()
+  const yargsA = yargs
+    .usage(USAGE)
+    .strict()
+    .demandCommand(1, 1)
+    .recommendCommands()
   const yargsB = COMMANDS.reduce(addCommand, yargsA)
-  return yargsB
+  const yargsC = yargsB.completion()
+  return yargsC
 }
+
+const COMMON_USAGE = `Unix permissions can take several shapes:
+  - octal, e.g. "755"
+  - same as stat output, e.g. "drwx-wx---"
+  - symbolic, i.e. same as chmod input, e.g. "a=rw"`
 
 const USAGE = `$0 COMMAND PERMISSIONS...
 
-Convert, test and manipulate Unix permissions.`
+Convert, test and manipulate Unix permissions.
+
+${COMMON_USAGE}`
 
 const addCommand = function(yargsA, { command, describe, examples }) {
   const builder = buildCommand.bind(null, { command, describe, examples })
@@ -22,7 +34,9 @@ const addCommand = function(yargsA, { command, describe, examples }) {
 }
 
 const buildCommand = function({ command, describe, examples }, yargsA) {
-  const yargsB = yargsA.usage(`$0 ${command}\n\n${describe}`).strict()
+  const yargsB = yargsA
+    .usage(`$0 ${command}\n\n${describe}\n\n${COMMON_USAGE}`)
+    .strict()
   const yargsC = addExamples(yargsB, examples)
   return yargsC
 }
