da1b6f70e775deb793b797d901a58ae81fc514b9
ehmicky <ehmicky@gmail.com>
2018-12-28 17:00:00 +0200

    Add comments

--- src/functions/set.js
+++ src/functions/set.js
@@ -2,6 +2,9 @@
 
 const { binaryMap } = require('../helpers')
 
+// Set permissions from `nodesMapA` to `nodesMap`
+// Missing permissions of `nodesMapA` are ignored
+// `+` or `-` permissions of `nodesMapA` are set as `+` or `-` to `nodesMap`
 const setMap = function(nodesMap, nodesMapA) {
   return { ...nodesMap, ...nodesMapA }
 }
--- src/functions/unset.js
+++ src/functions/unset.js
@@ -5,6 +5,9 @@ const { binaryMap } = require('../helpers')
 const { setMap } = require('./set')
 const { notMap } = require('./not')
 
+// Unset permissions from `nodesMapA` to `nodesMap`
+// Missing permissions of `nodesMapA` are ignored
+// `+` or `-` permissions of `nodesMapA` are set as `-` or `+` to `nodesMap`
 const unsetMap = function(nodesMap, nodesMapA) {
   return setMap(nodesMap, notMap(nodesMapA))
 }
