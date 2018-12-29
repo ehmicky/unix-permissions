10e577c78657a08c86f460d39ac5b2c49b8b2ebf
ehmicky <ehmicky@gmail.com>
2018-12-29 17:00:00 +0200

    Try fixing codecov

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-'use strict'
 // There is a bug with codecov that counts empty lines here as missing lines
-// eslint-disable-next-line padding-line-between-statements
+// eslint-disable-next-line strict
 module.exports = require('./dist')
