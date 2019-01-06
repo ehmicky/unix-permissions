1fc5ef6e0f8088419116ab40dbeda834b6fd4028
ehmicky <ehmicky@gmail.com>
2019-01-06 17:00:00 +0200

    Idempotence test for contain()

--- test/contain.js
+++ test/contain.js
@@ -1,10 +1,32 @@
 'use strict'
 
-const { CONTAIN_DATA, performTests } = require('./helpers')
+const test = require('ava')
+
+const { contain } = require('../localpack')
+
+const {
+  PARSE_DATA,
+  CONTAIN_DATA,
+  performTests,
+  normalizeArg,
+} = require('./helpers')
 
 performTests({
   title: ([arg, ...args]) =>
     `should test whether ${arg} contains ${args.join(' ')}`,
   command: 'contain',
   data: CONTAIN_DATA,
+})
+
+PARSE_DATA.forEach(({ type, args: [arg], title }) => {
+  // eslint-disable-next-line max-nested-callbacks
+  test(`[${type}] should 'contain' itself with ${title}`, t => {
+    const argA = normalizeArg({ t, arg })
+
+    if (argA === undefined) {
+      return
+    }
+
+    t.true(contain(argA, argA))
+  })
 })
