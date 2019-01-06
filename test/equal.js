b6781628d7f192233102c672b90df8b2c1bbc68c
ehmicky <ehmicky@gmail.com>
2019-01-06 17:00:00 +0200

    Add idempotence test for equal()

--- test/equal.js
+++ test/equal.js
@@ -1,10 +1,32 @@
 'use strict'
 
-const { EQUAL_DATA, performTests } = require('./helpers')
+const test = require('ava')
+
+const { equal } = require('../localpack')
+
+const {
+  PARSE_DATA,
+  EQUAL_DATA,
+  performTests,
+  normalizeArg,
+} = require('./helpers')
 
 performTests({
   title: ([arg, ...args]) =>
     `should test whether ${arg} equals ${args.join(' ')}`,
   command: 'equal',
   data: EQUAL_DATA,
+})
+
+PARSE_DATA.forEach(({ type, args: [arg], title }) => {
+  // eslint-disable-next-line max-nested-callbacks
+  test(`[${type}] should 'equal' itself with ${title}`, t => {
+    const argA = normalizeArg({ t, arg })
+
+    if (argA === undefined) {
+      return
+    }
+
+    t.true(equal(argA, argA))
+  })
 })
