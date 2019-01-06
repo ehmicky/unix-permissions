f611523c5d4000ca4dcbcee9a2dd8a6e29649f42
ehmicky <ehmicky@gmail.com>
2019-01-06 17:00:00 +0200

    Add idempotence tests for positive()

--- test/positive.js
+++ test/positive.js
@@ -1,9 +1,33 @@
 'use strict'
 
-const { POSITIVE_DATA, performTests } = require('./helpers')
+const test = require('ava')
+
+const { positive } = require('../localpack')
+
+const {
+  PARSE_DATA,
+  POSITIVE_DATA,
+  performTests,
+  normalizeArg,
+} = require('./helpers')
 
 performTests({
   title: args => `should return positive ${args}`,
   command: 'positive',
   data: POSITIVE_DATA,
+})
+
+PARSE_DATA.forEach(({ type, args: [arg], title }) => {
+  // eslint-disable-next-line max-nested-callbacks
+  test(`[${type}] should have idempotent 'positive' with ${title}`, t => {
+    const argA = normalizeArg({ t, arg })
+
+    if (argA === undefined) {
+      return
+    }
+
+    const argB = positive(argA)
+    const argC = positive(argB)
+    t.deepEqual(argB, argC)
+  })
 })
