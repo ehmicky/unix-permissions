20bb62aaba730905ba137dd6656df574a1678211
ehmicky <ehmicky@gmail.com>
2019-01-06 17:00:00 +0200

    Add idempotent test for not()

--- test/not.js
+++ test/not.js
@@ -1,9 +1,34 @@
 'use strict'
 
-const { SIMPLE_DATA, performTests } = require('./helpers')
+const test = require('ava')
+
+const { normalize, not } = require('../localpack')
+
+const { SIMPLE_DATA, PARSE_DATA, performTests } = require('./helpers')
 
 performTests({
   title: args => `should negate ${args}`,
   command: 'not',
   data: SIMPLE_DATA,
 })
+
+PARSE_DATA.forEach(({ type, args: [arg], title }) => {
+  // eslint-disable-next-line max-nested-callbacks
+  test(`[${type}] should have idempotent 'not' with ${title}`, t => {
+    const argA = normalizeArg({ arg })
+
+    if (argA === undefined) {
+      t.true(true)
+      return
+    }
+
+    const argB = not(not(argA))
+    t.deepEqual(argA, argB)
+  })
+})
+
+const normalizeArg = function({ arg }) {
+  try {
+    return normalize(arg)
+  } catch (error) {}
+}
