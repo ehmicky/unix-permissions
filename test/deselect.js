7690401f89bf18f2881efc24f67510faa5c4b21c
ehmicky <ehmicky@gmail.com>
2019-01-06 17:00:00 +0200

    Add idempotence tests for select()

--- test/deselect.js
+++ test/deselect.js
@@ -1,10 +1,37 @@
 'use strict'
 
-const { DESELECT_DATA, performTests } = require('./helpers')
+const test = require('ava')
+
+const { select, deselect } = require('../localpack')
+
+const {
+  PARSE_DATA,
+  DESELECT_DATA,
+  performTests,
+  normalizeArg,
+} = require('./helpers')
 
 performTests({
   title: ({ category, type, title }) =>
     `[${type}] should deselect.${category} ${title}`,
   command: ({ category }) => `deselect.${category}`,
   data: DESELECT_DATA,
+})
+
+PARSE_DATA.forEach(({ type, args: [arg], title }) => {
+  // eslint-disable-next-line max-nested-callbacks
+  test(`[${type}] should have idempotent 'select' and 'deselect' with ${title}`, t => {
+    const argA = normalizeArg({ t, arg })
+
+    if (argA === undefined) {
+      return
+    }
+
+    const argB = select.user(argA)
+    const argC = deselect.user(argB)
+    const argD = select.user(argC)
+    t.deepEqual(argB, argD)
+    const argE = deselect.user(argD)
+    t.deepEqual(argC, argE)
+  })
 })
