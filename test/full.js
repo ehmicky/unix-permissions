2d594a16eb48375282261a3dfce2c4d980e8f2c3
ehmicky <ehmicky@gmail.com>
2019-01-06 17:00:00 +0200

    Remove full()

--- test/full.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const { SIMPLE_DATA, performTests } = require('./helpers')
-
-performTests({
-  title: args => `should return full ${args}`,
-  command: 'full',
-  data: SIMPLE_DATA,
-})
