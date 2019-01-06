b5616cec7cc4929fb0960d66406f10f640d292b2
ehmicky <ehmicky@gmail.com>
2019-01-06 17:00:00 +0200

    Add idempotence test for convert()

--- test/parse.js
+++ test/parse.js
@@ -1,9 +1,54 @@
 'use strict'
 
-const { PARSE_DATA, performTests } = require('./helpers')
+const test = require('ava')
+
+const { convert } = require('../localpack')
+
+const { PARSE_DATA, performTests, normalizeArg } = require('./helpers')
 
 performTests({
   title: ({ type, title }) => `[${type}] should parse ${title}`,
   command: 'convert.symbolic',
   data: PARSE_DATA,
 })
+
+PARSE_DATA.forEach(({ type, args: [arg], title }) => {
+  // eslint-disable-next-line max-nested-callbacks
+  Object.keys(convert).forEach(typeA => {
+    // eslint-disable-next-line max-nested-callbacks
+    test(`[${type}] should have idempotent 'convert.${typeA}' with ${title}`, t => {
+      const argA = normalizeArg({ t, arg })
+
+      if (argA === undefined) {
+        return
+      }
+
+      if (isLossy(type, typeA)) {
+        t.true(true)
+        return
+      }
+
+      const argB = convert[typeA](argA)
+      const argC = convert[type](argB)
+      t.deepEqual(argA, argC)
+    })
+  })
+})
+
+// Conversion between some types loses information
+const isLossy = function(type, typeA) {
+  return LOSSY_CONVERSIONS.some(
+    types => types[0] === type && types[1] === typeA,
+  )
+}
+
+const LOSSY_CONVERSIONS = [
+  ['symbolic', 'number'],
+  ['symbolic', 'stat'],
+  ['symbolic', 'octal'],
+  ['object', 'number'],
+  ['object', 'stat'],
+  ['object', 'octal'],
+  ['octal', 'number'],
+  ['octal', 'stat'],
+]
