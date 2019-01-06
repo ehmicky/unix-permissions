713f43be487c977a2e67d43e2168bfb3d02619cf
ehmicky <ehmicky@gmail.com>
2019-01-06 17:00:00 +0200

    Add idempotence test for normalize()

--- test/normalize.js
+++ test/normalize.js
@@ -1,9 +1,27 @@
 'use strict'
 
-const { PARSE_DATA, performTests } = require('./helpers')
+const test = require('ava')
+
+const { normalize } = require('../localpack')
+
+const { PARSE_DATA, performTests, normalizeArg } = require('./helpers')
 
 performTests({
   title: ({ type, title }) => `[${type}] should normalize ${title}`,
   command: 'normalize',
   data: PARSE_DATA,
+})
+
+PARSE_DATA.forEach(({ type, args: [arg], title }) => {
+  // eslint-disable-next-line max-nested-callbacks
+  test(`[${type}] should have idempotent 'normalize' with ${title}`, t => {
+    const argA = normalizeArg({ t, arg })
+
+    if (argA === undefined) {
+      return
+    }
+
+    const argB = normalize(argA)
+    t.deepEqual(argA, argB)
+  })
 })
