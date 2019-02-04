a956ebb8b4a4a45dcc1c8db38b516b18be62d8f2
ehmicky <ehmicky@gmail.com>
2019-02-04 10:00:00 +0100

    Remove escape code with new Yargs release

--- src/cli/escape.js
+++ src/cli/escape.js
@@ -1,28 +1,17 @@
 'use strict'
 
-// We prepend spaces to some arguments in order to escape them.
-// Otherwise `yargs`:
-//   - transtypes `octal` to `number`
-//   - strips `+`
-//   - fails parsing with `stat` and `symbolic` (like `-xr`)
+// We prepend spaces to arguments starting with `-` in order to escape them.
+// Otherwise `yargs` fails parsing with `stat` and `symbolic` (like `-xr`)
 const escapeArgs = function(args) {
   return args.map(escapeArg)
 }
 
 const escapeArg = function(arg) {
-  if (shouldPrependSpace(arg)) {
+  if (shouldEscapeMinus(arg)) {
     return ` ${arg}`
   }
 
   return arg
-}
-
-const shouldPrependSpace = function(arg) {
-  return isOctal(arg) || arg.startsWith('+') || shouldEscapeMinus(arg)
-}
-
-const isOctal = function(arg) {
-  return Number.isInteger(Number(arg))
 }
 
 const shouldEscapeMinus = function(arg) {
