d337c9acabf5803e3bfe67ea97d62f1f411a6853
ehmicky <ehmicky@gmail.com>
2019-02-14 10:00:00 +0100

    Fix memoization not working for permissions objects

--- src/moize.js
+++ src/moize.js
@@ -7,7 +7,7 @@ const { mapValues } = require('./utils')
 // Moize a function, or an object containing functions
 const moizeFuncs = function(value) {
   if (typeof value === 'function') {
-    return moize(value)
+    return moize(value, { isDeepEqual: true })
   }
 
   return mapValues(value, moizeFuncs)
