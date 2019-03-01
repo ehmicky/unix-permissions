b6096df5abfa6eb179d29f12ab5b572e9b7af336
ehmicky <ehmicky@gmail.com>
2019-03-01 10:00:00 +0100

    Use new build task

--- test/helpers/cli.js
+++ test/helpers/cli.js
@@ -2,7 +2,7 @@
 
 const execa = require('execa')
 
-const BINARY_PATH = `${__dirname}/../../dist/bin/index.js`
+const BINARY_PATH = `${__dirname}/../../dist/src/bin/index.js`
 
 // Test that CLI output and exit code is same as programmatic output and
 // exception throwing
