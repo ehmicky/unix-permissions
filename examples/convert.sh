fd6cf02c743e689eb7162eff7e65fd2908b4d543
ehmicky <ehmicky@gmail.com>
2019-03-15 10:00:00 +0100

    Fix example

--- examples/convert.sh
+++ examples/convert.sh
@@ -2,7 +2,7 @@
 
 build/src/bin/index.js convert.symbolic 111 # a=x
 
-build/src/bin/index.js positive $(unix-permissions convert.symbolic 111) # a+x
+build/src/bin/index.js positive $(build/src/bin/index.js convert.symbolic 111) # a+x
 
 build/src/bin/index.js convert.octal o+x # +0001
 
