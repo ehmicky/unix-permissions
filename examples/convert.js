035f1b42a4c245ea93ae15d20dc6ae186f5edcf2
ehmicky <ehmicky@gmail.com>
2019-03-16 10:00:00 +0100

    Update example

--- examples/convert.js
+++ examples/convert.js
@@ -13,14 +13,13 @@ const unixPermissions = require('..')
 const { convert, positive } = unixPermissions
 
 const resultA = convert.symbolic('111') // 'a=x'
+console.log(resultA)
 
 const resultB = positive(convert.symbolic('111')) // 'a+x'
+console.log(resultB)
 
 const resultC = convert.octal('o+x') // '+0001'
+console.log(resultC)
 
 const resultD = convert.octal('o=x') // '0001'
-
-console.log(resultA)
-console.log(resultB)
-console.log(resultC)
 console.log(resultD)
