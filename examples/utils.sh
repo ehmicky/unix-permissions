dbbbfdf726a5ba23db26a99a785d3775e772d405
ehmicky <ehmicky@gmail.com>
2019-03-20 10:00:00 +0100

    Improve examples utilities

--- examples/utils.sh
+++ examples/utils.sh
@@ -2,5 +2,6 @@
 # Ignore this file, this is only needed for internal purposes.
 # We create an alias so that examples look the same as if the library was
 # directly installed.
+binaryName="unix-permissions"
 shopt -s expand_aliases
-alias unix-permissions="$(dirname "$BASH_SOURCE")/../build/src/bin/index.js"
+alias "$binaryName"="$(dirname "$BASH_SOURCE")/../build/src/bin/index.js"
