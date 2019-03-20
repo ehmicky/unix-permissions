0e04b812894a5cf3d48e54840dde6cbbe86d4138
ehmicky <ehmicky@gmail.com>
2019-03-20 10:00:00 +0100

    Update utils.sh

--- examples/utils.sh
+++ examples/utils.sh
@@ -2,6 +2,10 @@
 # Ignore this file, this is only needed for internal purposes.
 # We create an alias so that examples look the same as if the library was
 # directly installed.
-binaryName="unix-permissions"
+
+projectRoot="$(readlink -f "$(dirname "$BASH_SOURCE")/..")"
+binaryName="$(basename "$projectRoot")"
+pathToBinary="build/src/bin/index.js"
+
 shopt -s expand_aliases
-alias "$binaryName"="$(dirname "$BASH_SOURCE")/../build/src/bin/index.js"
+alias "$binaryName"="$projectRoot/$pathToBinary"
