8fda14028dccca88761efa967499bdbe84a92df0
ehmicky <ehmicky@gmail.com>
2019-03-20 10:00:00 +0100

    Add online demo for Bash

--- examples/convert.sh
+++ examples/convert.sh
@@ -3,6 +3,8 @@
 # This file can be directly run:
 #   - first install `unix-permissions`
 #   - then `bash node_modules/unix-permissions/examples/convert.sh`
+# An online demo is also available at:
+#   https://repl.it/@ehmicky/unix-permissions
 
 # Ignore the following line: this is only needed for internal purposes.
 . "$(dirname "$BASH_SOURCE")/utils.sh"
