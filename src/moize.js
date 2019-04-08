6e866b33b104aa60594e8336f3e8435e9282977f
ehmicky <ehmicky@gmail.com>
2019-04-08 10:00:00 +0200

    Use keep-func-props

--- src/moize.js
+++ src/moize.js
@@ -1,11 +1,14 @@
 import moize from 'moize'
+import keepFuncProps from 'keep-func-props'
 
 import { mapValues } from './utils.js'
+
+const kMoize = keepFuncProps(moize)
 
 // Moize a function, or an object containing functions
 export const moizeFuncs = function(value) {
   if (typeof value === 'function') {
-    return moize(value, { isDeepEqual: true })
+    return kMoize(value, { isDeepEqual: true })
   }
 
   return mapValues(value, moizeFuncs)
