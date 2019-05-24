fee6f73acce24d7fb40c94ca01d01ae208c53d7b
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/equal.js
+++ test/equal.js
@@ -7,9 +7,8 @@ import { VALID_PARSE_DATA } from './helpers/data/parse/main.js'
 import { EQUAL_DATA } from './helpers/data/equal.js'
 
 EQUAL_DATA.forEach(datum => {
-  const title = ([arg, ...args]) =>
-    `should test whether ${arg} equals ${args.join(' ')}`
-  test(title(datum), t => testCommand({ datum, command: 'equal', t }))
+  test(`equal() ${datum.map(JSON.stringify).join(' ')}`, t =>
+    testCommand({ datum, command: 'equal', t }))
 })
 
 VALID_PARSE_DATA.forEach(({ args: [arg] }) => {
