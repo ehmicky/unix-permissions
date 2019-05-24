be10c4dc19155ca9d3a2f53d7639437cb8c60a28
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/convert.js
+++ test/convert.js
@@ -2,32 +2,12 @@ import test from 'ava'
 
 import { convert, normalize } from '../src/main.js'
 
-import { VALID_CONVERT_DATA } from './helpers/data/convert.js'
+import { CONVERT_DATA } from './helpers/data/convert.js'
 
-// Conversion between some types loses information
-const isConvertible = function({ type, otherType }) {
-  return LOSSY_CONVERSIONS.every(
-    ([typeA, otherTypeA]) => typeA !== type || otherTypeA !== otherType,
-  )
-}
-
-const LOSSY_CONVERSIONS = [
-  ['symbolic', 'number'],
-  ['symbolic', 'stat'],
-  ['symbolic', 'octal'],
-  ['object', 'number'],
-  ['object', 'stat'],
-  ['object', 'octal'],
-  ['octal', 'number'],
-  ['octal', 'stat'],
-]
-
-VALID_CONVERT_DATA.filter(isConvertible).forEach(
-  ({ args: [arg], type, otherType }) => {
-    test(`should have idempotent 'convert' ${otherType} ${JSON.stringify(
-      arg,
-    )}`, t => {
-      t.deepEqual(normalize(arg), convert[type](convert[otherType](arg)))
-    })
-  },
-)
+CONVERT_DATA.forEach(({ args: [arg], type, otherType }) => {
+  test(`should have idempotent 'convert' ${otherType} ${JSON.stringify(
+    arg,
+  )}`, t => {
+    t.deepEqual(normalize(arg), convert[type](convert[otherType](arg)))
+  })
+})
--- test/helpers/data/convert.js
+++ test/helpers/data/convert.js
@@ -4,11 +4,28 @@ import { isValid } from '../valid.js'
 import { PARSE_DATA } from './parse/main.js'
 
 const getConvertData = function() {
-  return Object.keys(convert).flatMap(otherType =>
-    PARSE_DATA.map(args => ({ ...args, otherType })),
+  return Object.keys(convert)
+    .flatMap(otherType => PARSE_DATA.map(args => ({ ...args, otherType })))
+    .filter(isValid)
+    .filter(isConvertible)
+}
+
+// Conversion between some types loses information
+const isConvertible = function({ type, otherType }) {
+  return LOSSY_CONVERSIONS.every(
+    ([typeA, otherTypeA]) => typeA !== type || otherTypeA !== otherType,
   )
 }
 
-export const CONVERT_DATA = getConvertData()
+const LOSSY_CONVERSIONS = [
+  ['symbolic', 'number'],
+  ['symbolic', 'stat'],
+  ['symbolic', 'octal'],
+  ['object', 'number'],
+  ['object', 'stat'],
+  ['object', 'octal'],
+  ['octal', 'number'],
+  ['octal', 'stat'],
+]
 
-export const VALID_CONVERT_DATA = CONVERT_DATA.filter(isValid)
+export const CONVERT_DATA = getConvertData()
