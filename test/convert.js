36b004658d60e16d64fbb26f67f39571465210b7
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/convert.js
+++ test/convert.js
@@ -5,9 +5,7 @@ import { convert, normalize } from '../src/main.js'
 import { CONVERT_DATA } from './helpers/data/convert.js'
 
 CONVERT_DATA.forEach(({ arg, type, otherType }) => {
-  test(`should have idempotent 'convert' ${otherType} ${JSON.stringify(
-    arg,
-  )}`, t => {
+  test(`convert() idempotence ${JSON.stringify({ arg, otherType })}`, t => {
     t.deepEqual(normalize(arg), convert[type](convert[otherType](arg)))
   })
 })
