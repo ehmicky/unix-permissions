5d8171f6a14f3156e0b799c72014b36fe46a0a58
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/parse.js
+++ test/parse.js
@@ -5,7 +5,7 @@ import { PARSE_DATA, stringify } from './helpers/data/parse/main.js'
 
 PARSE_DATA.forEach(datum => {
   const { type, args: [arg] } = datum
-  test(`[${type}] should parse ${stringify(arg)}`, t =>
+  test(`parsing ${type} ${stringify(arg)}`, t =>
     testCommand({ datum, command: 'convert.symbolic', t }),
   )
 })
