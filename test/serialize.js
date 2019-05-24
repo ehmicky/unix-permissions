9ec859b848cad84a77bb1f90a10cb631a8e514d5
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/serialize.js
+++ test/serialize.js
@@ -1,7 +1,6 @@
 import test from 'ava'
 
 import { testCommand } from './helpers/command.js'
-import { stringify } from './helpers/data/parse/main.js'
 import { SERIALIZE_DATA } from './helpers/data/serialize.js'
 
 SERIALIZE_DATA.forEach(datum => {
@@ -9,8 +8,7 @@ SERIALIZE_DATA.forEach(datum => {
     type,
     args: [arg],
   } = datum
-  const title = () => `serialize ${type} ${JSON.stringify((arg))}`
-  test(title(datum), t =>
-    testCommand({ datum, command: ({ type }) => `convert.${type}`, t }),
+  test(`serialize ${type} ${JSON.stringify(arg)}`, t =>
+    testCommand({ datum, command: `convert.${type}`, t }),
   )
 })
