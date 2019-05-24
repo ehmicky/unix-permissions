784fd4b7d091ed607baf79c9904136faaca2c22f
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/invert.js
+++ test/invert.js
@@ -4,6 +4,5 @@ import { testCommand } from './helpers/command.js'
 import { SIMPLE_DATA } from './helpers/data/simple.js'
 
 SIMPLE_DATA.forEach(datum => {
-  const title = args => `should invert ${args}`
-  test(title(datum), t => testCommand({ datum, command: 'invert', t }))
+  test(`invert() ${datum}`, t => testCommand({ datum, command: 'invert', t }))
 })
