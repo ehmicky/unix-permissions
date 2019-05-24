71067dcacdb83ec738fd25fd5c4257484c4bf6b1
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/helpers/name.js
+++ test/helpers/name.js
@@ -1,17 +1,3 @@
-// Retrieve a different test name for each iteration
-export const getTestName = function(name, { type, otherType, category, title }) {
-  const prefix = getPrefix({ type, otherType, category })
-
-  return `${prefix} ${name} with ${title}`
-}
-
-const getPrefix = function({ type, otherType, category }) {
-  return [type, otherType, category]
-    .filter(Boolean)
-    .map(addBracket)
-    .join(' ')
-}
-
-const addBracket = function(name) {
-  return `[${name}]`
+export const getTestName = function(name, datum) {
+  return `${name} ${JSON.stringify(datum)}`
 }
