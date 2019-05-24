91e0bee278e1deddaeb8652ab204fce97de47cb8
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Fix linting

--- test/set.js
+++ test/set.js
@@ -4,6 +4,6 @@ import { testCommand } from './helpers/command.js'
 import { SET_DATA } from './helpers/data/set.js'
 
 SET_DATA.forEach(datum => {
-  test(`set() ${args.map(JSON.stringify).join(' ')}`, t =>
+  test(`set() ${datum.map(JSON.stringify).join(' ')}`, t =>
     testCommand({ datum, command: 'set', t }))
 })
