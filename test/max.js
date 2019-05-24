40bbc7cc0879c7ce9f87fa8123b952953013e6f2
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/max.js
+++ test/max.js
@@ -4,6 +4,5 @@ import { testCommand } from './helpers/command.js'
 import { MIN_MAX_DATA } from './helpers/data/min_max.js'
 
 MIN_MAX_DATA.forEach(datum => {
-  const title = args => `should return maximum ${args}`
-  test(title(datum), t => testCommand({ datum, command: 'max', t }))
+  test(`max() ${datum}`, t => testCommand({ datum, command: 'max', t }))
 })
