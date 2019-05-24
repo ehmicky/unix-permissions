64ef98872f82304e179925fe6ec68ae6ee527798
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/set.js
+++ test/set.js
@@ -4,6 +4,6 @@ import { testCommand } from './helpers/command.js'
 import { SET_DATA } from './helpers/data/set.js'
 
 SET_DATA.forEach(datum => {
-  const title = ([arg, ...args]) => `should set ${arg} with ${args.join(' ')}`
-  test(title(datum), t => testCommand({ datum, command: 'set', t }))
+  test(`set() ${args.map(JSON.stringify).join(' ')}`, t =>
+    testCommand({ datum, command: 'set', t }))
 })
