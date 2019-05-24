f9b3968d47aa0e18663746db7194fcd17694fcef
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/max.js
+++ test/max.js
@@ -4,5 +4,5 @@ import { testCommand } from './helpers/command.js'
 import { MIN_MAX_DATA } from './helpers/data/min_max.js'
 
 MIN_MAX_DATA.forEach(datum => {
-  test(`max() ${datum}`, t => testCommand({ datum, command: 'max', t }))
+  test(`max() ${JSON.stringify(datum)}`, t => testCommand({ datum, command: 'max', t }))
 })
