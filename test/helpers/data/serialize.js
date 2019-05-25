dcb94a0313f13221efcba3b75625b105a96a9f08
ehmicky <ehmicky@gmail.com>
2019-05-25 10:00:00 +0200

    Refactoring

--- test/helpers/data/serialize.js
+++ test/helpers/data/serialize.js
@@ -2,9 +2,8 @@ import { convert } from '../../../src/main.js'
 
 import { SIMPLE_DATA } from './simple.js'
 
-const getSerializeData = function() {
-  const types = Object.keys(convert)
-  return SIMPLE_DATA.flatMap(arg => types.map(type => ({ type, arg })))
-}
+const types = Object.keys(convert)
 
-export const SERIALIZE_DATA = getSerializeData()
+export const SERIALIZE_DATA = SIMPLE_DATA.flatMap(arg =>
+  types.map(type => ({ type, arg })),
+)
