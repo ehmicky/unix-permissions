f67002e1c2d6950868d2e0658ddac24aa066a55e
ehmicky <ehmicky@gmail.com>
2019-05-26 10:00:00 +0200

    Refactoring

--- test/convert.js
+++ test/convert.js
@@ -4,7 +4,29 @@ import { convert, normalize } from '../src/main.js'
 
 import { CONVERT_DATA } from './helpers/data/convert.js'
 
+// Conversion between some types loses information
+const isLossy = function(type, otherType) {
+  return LOSSY_CONVERSIONS.some(
+    ([typeA, otherTypeA]) => typeA === type && otherTypeA === otherType,
+  )
+}
+
+const LOSSY_CONVERSIONS = [
+  ['symbolic', 'number'],
+  ['symbolic', 'stat'],
+  ['symbolic', 'octal'],
+  ['object', 'number'],
+  ['object', 'stat'],
+  ['object', 'octal'],
+  ['octal', 'number'],
+  ['octal', 'stat'],
+]
+
 CONVERT_DATA.forEach(({ arg, type, otherType }) => {
+  if (isLossy(type, otherType)) {
+    return
+  }
+
   test(`convert() idempotence ${JSON.stringify({ arg, otherType })}`, t => {
     t.deepEqual(normalize(arg), convert[type](convert[otherType](arg)))
   })
--- test/helpers/data/convert.js
+++ test/helpers/data/convert.js
@@ -1,24 +1,6 @@
 import { VALID_PARSE_DATA } from './parse/main.js'
 import { TYPES } from './types.js'
 
-// Conversion between some types loses information
-const isConvertible = function({ type, otherType }) {
-  return LOSSY_CONVERSIONS.every(
-    ([typeA, otherTypeA]) => typeA !== type || otherTypeA !== otherType,
-  )
-}
-
-const LOSSY_CONVERSIONS = [
-  ['symbolic', 'number'],
-  ['symbolic', 'stat'],
-  ['symbolic', 'octal'],
-  ['object', 'number'],
-  ['object', 'stat'],
-  ['object', 'octal'],
-  ['octal', 'number'],
-  ['octal', 'stat'],
-]
-
 export const CONVERT_DATA = VALID_PARSE_DATA.flatMap(({ arg, type }) =>
   TYPES.map(otherType => ({ arg, type, otherType })),
-).filter(isConvertible)
+)
