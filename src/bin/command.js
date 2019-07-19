2582d4d2bcb965c7164152f2e23fc93e7f20f9e9
ehmicky <ehmicky@gmail.com>
2019-07-19 10:00:00 +0200

    Fix linting for no-unused-modules

--- src/bin/command.js
+++ src/bin/command.js
@@ -1,5 +1,4 @@
 // Retrieve main command, including dot notation like `convert.symbolic`
-// eslint-disable-next-line import/no-unused-modules
 export const getCommand = function({ unixPermissions, command }) {
   if (!command.includes('.')) {
     return unixPermissions[command]
