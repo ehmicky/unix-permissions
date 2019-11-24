f188c28f2c1ce3a60f28503467573166efaf2a05
ehmicky <ehmicky@gmail.com>
2019-11-24 19:27:39 +0100

    Warn when a new version is available

--- src/bin/main.js
+++ src/bin/main.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-import { exit } from 'process'
+import { exit, env } from 'process'
+
+import UpdateNotifier from 'update-notifier'
+import readPkgUp from 'read-pkg-up'
 
 import * as unixPermissions from '../main.js'
 
@@ -11,6 +14,8 @@ import { handleOutput } from './output.js'
 // Parse CLI arguments then run tasks
 const runCli = async function() {
   try {
+    await checkUpdate()
+
     const yargs = defineCli()
     const { command, args } = parseConfig({ yargs })
     const commandA = getCommand({ unixPermissions, command })
@@ -19,6 +24,12 @@ const runCli = async function() {
   } catch (error) {
     runCliHandler(error)
   }
+}
+
+const checkUpdate = async function() {
+  const { packageJson } = await readPkgUp({ cwd: __dirname, normalize: false })
+  const disabled = env.NODE_ENV === 'test'
+  UpdateNotifier({ pkg: packageJson, disabled }).notify()
 }
 
 // If an error is thrown, print error's description, then exit with exit code 1
