5023efad38e03d9079014fed27377d9ba8b29349
ehmicky <ehmicky@gmail.com>
2020-06-28 17:36:57 +0200

    Simplify CLI examples

--- src/bin/top.js
+++ src/bin/top.js
@@ -28,23 +28,12 @@ ${COMMON_USAGE}`
 
 const addCommand = function (yargsA, { command, describe, examples }) {
   const builder = buildCommand.bind(undefined, { command, describe, examples })
-  const yargsB = yargsA.command({ command, describe, builder })
-  const yargsC = addExamples(yargsB, examples)
-  return yargsC
+  return yargsA.command({ command, describe, builder }).example(examples)
 }
 
 const buildCommand = function ({ command, describe, examples }, yargsA) {
-  const yargsB = yargsA
+  return yargsA
     .usage(`$0 ${command}\n\n${describe}\n\n${COMMON_USAGE}`)
     .strict()
-  const yargsC = addExamples(yargsB, examples)
-  return yargsC
-}
-
-const addExamples = function (yargsA, examples) {
-  return examples.reduce(addExample, yargsA)
-}
-
-const addExample = function (yargsA, [example, exampleDescription]) {
-  return yargsA.example(`$0 ${example}`, exampleDescription)
+    .example(examples)
 }
