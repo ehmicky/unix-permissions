805dde1047d635078ee8ba9ba5cf2e1a20831d3d
ehmicky <ehmicky@gmail.com>
2021-01-03 23:43:27 +0100

    Simplify code

--- src/bin/top.js
+++ src/bin/top.js
@@ -3,16 +3,17 @@ import yargs from 'yargs'
 import { COMMANDS } from './commands.js'
 
 export const defineCli = function () {
-  const yargsA = yargs
-    .usage(USAGE)
-    .strict()
-    .demandCommand(1, 1)
-    .recommendCommands()
-    // Permissions cannot be input as numbers, only as octals
-    .parserConfiguration({ 'parse-numbers': false })
-  const yargsB = COMMANDS.reduce(addCommand, yargsA)
-  const yargsC = yargsB.completion()
-  return yargsC
+  return (
+    yargs
+      .command(COMMANDS.map(getCommand))
+      .demandCommand(1, 1)
+      .recommendCommands()
+      .usage(USAGE)
+      .strict()
+      // Permissions cannot be input as numbers, only as octals
+      .parserConfiguration({ 'parse-numbers': false })
+      .completion()
+  )
 }
 
 const COMMON_USAGE = `Unix permissions can take several types:
@@ -26,14 +27,14 @@ Convert, test and manipulate Unix permissions.
 
 ${COMMON_USAGE}`
 
-const addCommand = function (yargsA, { command, describe, examples }) {
-  const builder = buildCommand.bind(undefined, { command, describe, examples })
-  return yargsA.command({ command, describe, builder }).example(examples)
-}
-
-const buildCommand = function ({ command, describe, examples }, yargsA) {
-  return yargsA
-    .usage(`$0 ${command}\n\n${describe}\n\n${COMMON_USAGE}`)
-    .strict()
-    .example(examples)
+const getCommand = function ({ command, describe, examples }) {
+  return {
+    command,
+    describe,
+    builder: (commandYargs) =>
+      commandYargs
+        .usage(`$0 ${command}\n\n${describe}\n\n${COMMON_USAGE}`)
+        .example(examples)
+        .strict(),
+  }
 }
