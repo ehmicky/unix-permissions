41a88a25e8202a509be61383fdb956c579040e77
ehmicky <ehmicky@gmail.com>
2022-07-17 11:14:11 +0200

    Fix typo

--- src/converters.js
+++ src/converters.js
@@ -2,7 +2,7 @@ import { convert as convertValue } from './helpers.js'
 import { TYPES_MAP } from './types/main.js'
 import { mapValues } from './utils.js'
 
-// Retrieve a map of all types' convertion functions
+// Retrieve a map of all types' conversion functions
 const getConverters = function () {
   return mapValues(TYPES_MAP, getConverter)
 }
