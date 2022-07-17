dc134541eb9683b8a5d87c07fcd4527fb8f61c19
ehmicky <ehmicky@gmail.com>
2022-07-17 13:23:13 +0200

    Add PermissionOctal type

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,4 +1,43 @@
 /**
+ * Permission type used by [`chmod`](https://linux.die.net/man/1/chmod).
+ *
+ * Octal string where each digit represents a user class: `user`, `group` and
+ * `others`.
+ * Each digit's is a [bitfield](https://en.wikipedia.org/wiki/Bit_field)
+ * representing `read`, `write` and `execute`. Special permissions
+ * ([setuid](https://en.wikipedia.org/wiki/Setuid),
+ * [setgid](https://en.wikipedia.org/wiki/Setuid),
+ * [sticky](https://en.wikipedia.org/wiki/Sticky_bit)) can optionally be specified
+ * by prepending another digit.
+ *
+ * An operator can be prepended:
+ *  - `=` (default): unset omitted permissions
+ *  - `+`: leave omitted permissions as is
+ *  - `-`: unset specified permissions
+ *
+ * @example
+ * ```js
+ * console.log(convert.stat('720')) // 'rwx-w----'
+ * console.log(convert.stat('7000')) // '--S--S--T'
+ * console.log(convert.stat('\\720')) // 'rwx-w----'
+ * console.log(convert.stat('0720')) // 'rwx-w----'
+ * console.log(convert.stat('0o720')) // 'rwx-w----'
+ * console.log(convert.symbolic('+720')) // 'u+rwx,g+w'
+ * console.log(convert.symbolic('-720')) // 'u-rwx,g-w'
+ * console.log(convert.symbolic('=720')) // 'u=rwx,g=w,o='
+ * ```
+ */
+export type PermissionOctal =
+  | `${OctalStart}${OctalDigit}`
+  | `${OctalStart}${OctalDigit}${OctalDigit}`
+  | `${OctalStart}${OctalDigit}${OctalDigit}${OctalDigit}`
+  | `${OctalStart}${OctalDigit}${OctalDigit}${OctalDigit}${OctalDigit}`
+type OctalDigit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7'
+type OctalPrefix = '' | '0o' | '0' | '\\'
+type OctalOperator = '' | '=' | '+' | '-'
+type OctalStart = `${OctalOperator}${OctalPrefix}`
+
+/**
  *
  * @example
  * ```js
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,2 +1,27 @@
-import { convert } from 'unix-permissions'
-import { expectType, expectError } from 'tsd'
+import { convert, PermissionOctal } from 'unix-permissions'
+import {
+  expectType,
+  expectError,
+  expectAssignable,
+  expectNotAssignable,
+} from 'tsd'
+
+expectAssignable<PermissionOctal>('1')
+expectAssignable<PermissionOctal>('11')
+expectAssignable<PermissionOctal>('111')
+expectAssignable<PermissionOctal>('1111')
+expectNotAssignable<PermissionOctal>('11111')
+expectNotAssignable<PermissionOctal>('8')
+expectNotAssignable<PermissionOctal>('18')
+expectNotAssignable<PermissionOctal>('118')
+expectNotAssignable<PermissionOctal>('1118')
+expectNotAssignable<PermissionOctal>('a')
+expectAssignable<PermissionOctal>('0o1111')
+expectAssignable<PermissionOctal>('01111')
+expectAssignable<PermissionOctal>('\\1111')
+expectNotAssignable<PermissionOctal>('oo1111')
+expectAssignable<PermissionOctal>('+1111')
+expectAssignable<PermissionOctal>('=1111')
+expectAssignable<PermissionOctal>('-1111')
+expectNotAssignable<PermissionOctal>('/1111')
+expectAssignable<PermissionOctal>('-0o1111')
