7d6fd0e049d7c34e71d872d33a857b3828b280c6
ehmicky <ehmicky@gmail.com>
2022-12-11 17:19:24 +0100

    Fix CI tests

--- ava.config.js
+++ ava.config.js
@@ -1,1 +1,7 @@
-export { default } from '@ehmicky/dev-tasks/ava.config.js'
+import avaConfig from '@ehmicky/dev-tasks/ava.config.js'
+
+export default {
+  ...avaConfig,
+  // CI tests on Windows fail due to the amount of parallel tests otherwise
+  concurrency: 1,
+}
