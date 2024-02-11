b0315cc4bdfe0dc7320e9109e22f1d1511bf4333
ehmicky <ehmicky@gmail.com>
2024-02-11 13:46:33 +0000

    Fix linting

--- src/bin/top.js
+++ src/bin/top.js
@@ -1,7 +1,6 @@
 import { argv } from 'node:process'
 
 import yargs from 'yargs'
-// eslint-disable-next-line n/file-extension-in-import
 import { hideBin } from 'yargs/helpers'
 
 import { COMMANDS } from './commands.js'
